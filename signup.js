3441b2868fe05ff2f38bf11e4940d48d98915aa4
Our Matrimony <hemanthreddydubbaka@gmail.com>
2026-07-24 07:50:54 +0530

    Connect signup.js to Firebase

--- signup.js
+++ signup.js
@@ -2,27 +2,25 @@ import { auth, db } from "./firebase.js";
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-document.getElementById("signupBtn").addEventListener("click", async function () {
-
+document.getElementById("signupBtn").addEventListener("click", async () => {
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
   const phone = document.getElementById("phone").value;
+  const password = document.getElementById("password").value;
 
   try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = await createUserWithEmailAndPassword(auth, email, password);
 
-    await setDoc(doc(db, "users", userCredential.user.uid), {
-      name: name,
-      email: email,
-      phone: phone
+    await setDoc(doc(db, "users", user.user.uid), {
+      name,
+      email,
+      phone
     });
 
     alert("Registration Successful!");
-    window.location.href = "profile.html";
+    window.location.href = "login.html";
 
   } catch (error) {
     alert(error.message);
   }
-
 });
